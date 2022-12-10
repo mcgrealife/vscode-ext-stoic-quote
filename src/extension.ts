bfa95b9d4c505a20840fd8b3739cfed4ae278eb9
Michael McGreal <mcgrealife@gmail.com>
2022-12-10 11:26:06 -0500

    stoicQuote as modal!

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import fetch from 'node-fetch' // note: errors in node-fetch@3.3.0 // downgraded to ^2.6.7
 import * as vscode from 'vscode'
 
 // This method is called when your extension is activated
@@ -7,23 +8,36 @@ import * as vscode from 'vscode'
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "zencode" is now active!')
+
+  type StoicQuote = {
+    id: number
+    body: string
+    author: string
+  }
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('zencode.helloWorld', () => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World from zencode!', {
-      command: 'zencode.helloWorld',
-      title: 'test',
-    })
-    console.log('Hello World from zencode!')
-  })
+  let disposable = vscode.commands.registerCommand(
+    'zencode.fetchStoicQuote',
+    () => {
+      fetch('https://stoicquotesapi.com/v1/api/quotes/random').then((res) =>
+        res.json().then((res: StoicQuote) =>
+          vscode.window.showInformationMessage(
+            res.body,
+            {
+              modal: true,
+              detail: res.author,
+            },
+            { title: 'True' }
+          )
+        )
+      )
+    }
+  )
 
   context.subscriptions.push(disposable)
-  vscode.commands.executeCommand('zencode.helloWorld')
+  vscode.commands.executeCommand('zencode.fetchStoicQuote')
 }
 
 // This method is called when your extension is deactivated
